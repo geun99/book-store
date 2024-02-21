4c24790f649275c368e4af3221c440edf28560e5
geun99 <lich1115@naver.com>
2024-02-22 02:49:33 +0900

    헤더,푸터 완성 라우터 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Layout from "./components/layout/Layout";
+import Home from "./pages/Home";
+import { BookStoreThemeProvider } from "./Context/themeContext";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Error from "./components/common/Error";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <Layout>
+        <Home />
+      </Layout>
+    ),
+    errorElement: <Error />,
+  },
+  {
+    path: "/books",
+    element: (
+      <Layout>
+        <div>도서 목록</div>
+      </Layout>
+    ),
+  },
+]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BookStoreThemeProvider>
+      <RouterProvider router={router} />
+    </BookStoreThemeProvider>
   );
 }
 
---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+function Home() {
+  return (
+    <>
+      {/* <Title size="small" color="background">
+        제목 테스트
+      </Title>
+      <Button size="large" scheme="normal">
+        버튼 테스트
+      </Button>
+      <InputText placeholder="여기에 입력하세요" /> */}
+      <div>home body</div>
+    </>
+  );
+}
+
+export default Home;
